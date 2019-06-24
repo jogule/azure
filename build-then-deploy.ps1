7a477549e09af72a1d5d6deacb1c4b9ebb7f52c6
Jonathan Guzman Lemos <jonguz@microsoft.com>
2019-06-23 19:49:35 -0500

    myrg871 test

--- build-then-deploy.ps1
+++ build-then-deploy.ps1
@@ -10,7 +10,7 @@ $rgName = "myrg$Sufix"
 $rgName
 
 git commit -am "$rgName test"
-git push origin $environment
+git push github $environment
 
 New-AzResourceGroup -Name $rgName -Location 'East US 2'
 
